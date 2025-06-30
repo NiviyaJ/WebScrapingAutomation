877c659bd3009880f382a187af0e6c4a8accd747
Niviya Joy <niviyajoy@Niviyas-MacBook-Air.local>
2025-06-30 23:33:30 +0530

    ME_QA_XSCRAPE_DATA_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,16 +1,21 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.logging.Level;
+
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
@@ -21,7 +26,26 @@ public class TestCases {
      * TODO: Write your tests here with testng @Test annotation. 
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
+    @Test
+    public void testCase01(){
+        driver.get("https://www.scrapethissite.com/pages/");
+        WebElement hockeyLink = driver.findElement(By.partialLinkText("Hockey Teams"));
+        Wrappers.click(driver, hockeyLink);
+        Assert.assertTrue(driver.getCurrentUrl().endsWith("/pages/forms/"));
+        ArrayList<HashMap<String, String>> listOfData = Wrappers.navigatePageToCollectData(driver, 0.4,4);
+        Wrappers.writeToJSONFile(listOfData, "hockey-team-data");
+    }
 
+    @Test
+    public void testCase02(){
+        driver.get("https://www.scrapethissite.com/pages/");
+        WebElement oscarFilmLink = driver.findElement(By.partialLinkText("Oscar Winning Films"));
+        Wrappers.click(driver, oscarFilmLink);
+        Assert.assertTrue(driver.getCurrentUrl().endsWith("/pages/ajax-javascript/"));
+        ArrayList<HashMap<String, String>> listOfMovies = Wrappers.getTopMoviesEachYear(driver);
+        boolean status = Wrappers.writeToJSONFile(listOfMovies, "oscar-winner-data");
+        Assert.assertTrue(status, "File does not exist");
+    }
      
     /*
      * Do not change the provided methods unless necessary, they will help in automation and assessment
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -3,17 +3,140 @@ package demo.wrappers;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.idealized.Javascript;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
 import java.time.Duration;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    public static void click(WebDriver driver, WebElement elementToClick){
+        try {
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            // WebElement elementToClick = driver.findElement(locator);
+            js.executeScript("arguments[0].scrollIntoView(); arguments[0].click();", elementToClick);
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Exception occured: "+e.getMessage());
+        }
+    }
+
+    public static ArrayList<HashMap<String, String>> navigatePageToCollectData(WebDriver driver, double percent, int pageCount){
+        ArrayList<HashMap<String, String>> listOfData = new ArrayList<>();
+        try {
+            do{
+                WebElement nextBtn = driver.findElement(By.xpath("//a[@aria-label='Next']"));
+                click(driver, nextBtn);
+                //get list of all win% less than given parameter from the table
+                List<WebElement> winPercentList = driver.findElements(By.xpath("//td[contains(@class, 'pct') and text() < "+percent+ "]"));
+                
+                for(WebElement winColumn : winPercentList){
+                    
+                    HashMap<String, String> hm = new HashMap<>();
+                    WebElement teamNameCol = winColumn.findElement(By.xpath(".//preceding-sibling::td[@class='name']"));
+                    WebElement yearCol = winColumn.findElement(By.xpath(".//preceding-sibling::td[@class='year']"));
+
+                    long epoch = System.currentTimeMillis()/1000;
+                    hm.put("epoch_time", String.valueOf(epoch));
+                    hm.put("team_name", teamNameCol.getText());
+                    hm.put("year", yearCol.getText());
+                    hm.put("win%",winColumn.getText());
+                    listOfData.add(hm);
+                    // System.out.println(System.currentTimeMillis()/1000+" "+teamNameCol.getText()+" "+yearCol.getText()+" "+winColumn.getText());
+                }
+                
+                // Thread.sleep(2000);
+                pageCount--;
+            }while(pageCount > 0);
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Exception occured: "+e.getMessage());
+        }
+        return listOfData;
+    }
+
+    public static boolean writeToJSONFile(ArrayList<HashMap<String,String>> data, String fileName){
+        boolean fileExists = false;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            // String prettyJsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(data);
+            // System.out.println(prettyJsonString);
+            //Writing JSON on a file
+            String userDir = System.getProperty("user.dir");
+            File file = new File(userDir+"/src/test/resources/"+fileName+".json");
+            mapper.writerWithDefaultPrettyPrinter().writeValue(file, data);
+            fileExists = file.exists();
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Exception occured: "+e.getMessage());
+        }
+        return fileExists;
+    }
+
+    public static ArrayList<HashMap<String, String>> getTopMoviesEachYear(WebDriver driver){
+        ArrayList<HashMap<String, String>> listOfMovies = new ArrayList<>();
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+            //get list of years to click
+            List<WebElement> yearLinks = driver.findElements(By.className("year-link"));
+            
+            for(WebElement yearLink : yearLinks){
+                String year = yearLink.getText();
+                click(driver, yearLink);
+                wait.until(ExpectedConditions.presenceOfElementLocated(By.className("table")));
+                List<WebElement> tableRows = driver.findElements(By.xpath("//tr[@class='film' and position() <= 5]"));
+                // System.out.println("table row count: "+tableRows.size());
+                for(WebElement row : tableRows){
+                    HashMap<String, String> hm = new HashMap<>();
+                    long epoch = System.currentTimeMillis()/1000;
+                    boolean isWinner = true;
+                    WebElement titleElement = row.findElement(By.className("film-title"));
+                    WebElement nominationElement = row.findElement(By.className("film-nominations"));
+                    WebElement awardsElement = row.findElement(By.className("film-awards"));
+                    try {
+                        row.findElement(By.tagName("i"));
+                    } catch (Exception ex) {
+                        // TODO: handle exception
+                        isWinner = false;              
+                    }
+                    // List<WebElement> winner = row.findElements(By.tagName("i"));
+                    // if(winner.size() > 0)
+                    //     isWinner = true;
+                    hm.put("epoch_time", String.valueOf(epoch));
+                    hm.put("year", year);
+                    hm.put("title", titleElement.getText());
+                    hm.put("nominations", nominationElement.getText());
+                    hm.put("awards", awardsElement.getText());
+                    hm.put("isWinner", String.valueOf(isWinner));
+                    listOfMovies.add(hm);
+                    // System.out.println(epoch+" "+year+" "+titleElement.getText()+" "+nominationElement.getText()+" "+awardsElement.getText()+" "+isWinner);
+                }
+            }
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Exception occured: "+e.getMessage());
+        }
+        return listOfMovies;
+    }
 }
